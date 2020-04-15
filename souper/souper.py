08cc943e3c523255cb358840353e58813a749a0b
Josh Cundiff <josh.cundiff@gmail.com>
2020-04-15 11:36:07 -0400

    Pulled comments into readme

--- souper/souper.py
+++ souper/souper.py
@@ -1,9 +1,3 @@
-# This simple python program scrapes biblegateway.com for bible verses
-# and writes the output to a text file. The input text file was manually 
-# split into three seperate files with whitespace removed. This could be 
-# further improved to read from a single text input file, and then parse
-# the data into the appropriate lists.
-
 import requests
 from bs4 import BeautifulSoup
 
